5a22d83064af8c619e7d058077c2a593a0d88370
Area Dev <otavio.filho@aluno.uece.br>
2024-10-28 08:29:45 -0300

    teste
    teste

--- portalsalaberga/app/main/controllers/controller_cadastro/controller_pre_cadastro.php
+++ portalsalaberga/app/main/controllers/controller_cadastro/controller_pre_cadastro.php
@@ -9,7 +9,7 @@ if (isset($_POST['pre_cadastro']) && isset($_POST['preemail']) && isset($_POST['
     pre_cadastro($email, $cpf);
     
 } else {
-    header('location:../../views/autenticação/precadastro.php');
+    header('location:../../views/autenticação/precadastro.php');hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
 }
 
 if (isset($_GET['certo'])) {
