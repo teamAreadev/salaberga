fab13773465a5f08d60394cde6da702be2f668b1
Roger <cavalcanterogeer@gmail.com>
2024-10-29 11:01:53 -0300

    Criei a sessao para recsenha
    Criei uma sessão quando o usuario receber o link e entrar na pagina para recuperar a senha, ele recebera uma sessao que durará um tempo x, quando ele for recuperar novamente vai verificar se a sessao ainda esta criada, caso esteja ele não vai conseguir recuperar a senha

--- portalsalaberga/app/main/controllers/controller_recsenha/controoller_recSenha.php
+++ portalsalaberga/app/main/controllers/controller_recsenha/controoller_recSenha.php
@@ -1,8 +1,8 @@
 <?php 
 
-if(isset($_POST['recsenha']) && isset($_POST['phpemail']) && !empty($_POST['phpemail'])){
+if(isset($_POST['recsenha']) && isset($_POST['Email']) && !empty($_POST['Email'])){
 
-    $email = $_POST['phpemail'];
+    $email = $_POST['Email'];
 
     require('../../models/model_dados.php');
     recSenha($email);
--- portalsalaberga/app/main/views/autenticação/alteraSenha.php
+++ portalsalaberga/app/main/views/autenticação/alteraSenha.php
@@ -1,7 +1,12 @@
 
+<?php
+session_start();
+$_SESSION['recsenha'] = true;
+require_once('../../controllers/controller_sessao/verificar_sessao.php');
+    verificarSessao(3600);
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
