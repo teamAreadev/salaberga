34ff91a564d6d097ef17e85f4bad7d95e04a3ccd
matheus felix <matheus.dev91@gmail.com>
2024-11-19 09:35:18 -0300

    adicionando o estilo pagina success

--- portalsalaberga/app/subsystems/seeps2024/views/inicio.php
+++ portalsalaberga/app/subsystems/seeps2024/views/inicio.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>SS - Salaberga Selecione </title>
+      <title>SS - Salaberga Seleciona </title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="../assets/theme/js/modal.js"></script>
