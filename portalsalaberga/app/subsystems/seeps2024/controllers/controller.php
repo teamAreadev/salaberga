a830e8ac7e6b00ab1b0969b0c5e04959a3b967ec
DevRoger7 <cavalcanterogeer@gmail.com>
2024-11-19 19:59:25 -0300

    atualizando o calculo da media de ed fisica

--- portalsalaberga/app/subsystems/seeps2024/controllers/controller.php
+++ portalsalaberga/app/subsystems/seeps2024/controllers/controller.php
@@ -1,5 +1,4 @@
 <?php
-
 //cadastro
 
 function virg($num)
@@ -61,23 +60,23 @@ switch ($_POST['bairro']) {
 
     case 'Outros Bairros':
 
-        $bairro = '0';
+        $bairro = 0;
         break;
     case 'Outra Banda':
 
-        $bairro = '1';
+        $bairro = 1;
         break;
 };
 
-
+$d = 4;
 //6° ano
 
-if (!isset($_POST['ef6'])) {
-    $ef6 = 1;
-} else {
+if (!empty($_POST['ef6'])) {
     $ef6 = $_POST['ef6'];
+} else {
+    $ef6 = 0;
+    $d -= 1;
 }
-
 $lp6 = virg($_POST['lp6']);
 $ar6 = virg($_POST['a6']);
 $li6 = virg($_POST['i6']);
@@ -88,13 +87,12 @@ $hi6 = virg($_POST['h6']);
 $re6 = virg($_POST['r6']);
 
 //7° ano
-if (!isset($_POST['ef7'])) {
-    $ef7 = 1;
-} else {
+if (!empty($_POST['ef7'])) {
     $ef7 = $_POST['ef7'];
+} else {
+    $ef7 = 0;
+    $d -= 1;
 }
-
-
 $lp7 = virg($_POST['lp7']);
 $ar7 = virg($_POST['a7']);
 $li7 = virg($_POST['i7']);
@@ -106,13 +104,12 @@ $re7 = virg($_POST['r7']);
 
 //8° ano
 
-if (!isset($_POST['ef8'])) {
-    $ef8 = 1;
-} else {
+if (!empty($_POST['ef8'])) {
     $ef8 = $_POST['ef8'];
+} else {
+    $ef8 = 0;
+    $d -= 1;
 }
-
-
 $lp8 = virg($_POST['lp8']);
 $ar8 = virg($_POST['a8']);
 $li8 = virg($_POST['i8']);
@@ -123,10 +120,11 @@ $hi8 = virg($_POST['h8']);
 $re8 = virg($_POST['r8']);
 
 //9° ano
-
+$d9 = 4;
 //1 bimestre
-if (!isset($_POST['ef9_1'])) {
-    $ef9_1 = 1;
+if (empty($_POST['ef9_1'])) {
+    $ef9_1 = 0;
+    $d9 -= 1;
 } else {
     $ef9_1 = $_POST['ef9_1'];
 }
@@ -140,8 +138,9 @@ $a9_1 = virg($_POST['a9_1']);
 $r9_1 = virg($_POST['r9_1']);
 
 //2 bimestre
-if (!isset($_POST['ef9_2'])) {
-    $ef9_2 = 1;
+if (empty($_POST['ef9_2'])) {
+    $ef9_2 = 0;
+    $d9 -= 1;
 } else {
     $ef9_2 = $_POST['ef9_2'];
 }
@@ -155,8 +154,9 @@ $a9_2 = virg($_POST['a9_2']);
 $r9_2 = virg($_POST['r9_2']);
 
 //3 bimestre
-if (!isset($_POST['ef9_3'])) {
-    $ef9_3 = 1;
+if (empty($_POST['ef9_3'])) {
+    $ef9_3 = 0;
+    $d9 -= 1;
 } else {
     $ef9_3 = $_POST['ef9_3'];
 }
@@ -169,9 +169,11 @@ $i9_3 = virg($_POST['i9_3']);
 $a9_3 = virg($_POST['a9_3']);
 $r9_3 = virg($_POST['r9_3']);
 
+$n9 = 4;
 //4 bimestre
-if (!isset($_POST['ef9_4'])) {
-    $ef9_4 = 1;
+if (empty($_POST['ef9_4'])) {
+    $ef9_4 = 0;
+    $d9 -= 1;
 } else {
     $ef9_4 = $_POST['ef9_4'];
 }
@@ -180,78 +182,84 @@ if (isset($_POST['lp9_4']) && !empty($_POST['lp9_4'])) {
     $lp9_4 = virg($_POST['lp9_4']);
 } else {
     // Ação caso 'lp9_4' não esteja definido ou esteja vazio
-    $lp9_4 = 1;
+    $lp9_4 = 0;
+    $n9 = 3;
 }
 
 if (isset($_POST['m9_4']) && !empty($_POST['m9_4'])) {
     $m9_4 = virg($_POST['m9_4']);
 } else {
     // Ação caso 'm9_4' não esteja definido ou esteja vazio
-    $m9_4 = 1;
+    $m9_4 = 0;
 }
 
 if (isset($_POST['h9_4']) && !empty($_POST['h9_4'])) {
     $h9_4 = virg($_POST['h9_4']);
 } else {
     // Ação caso 'h9_4' não esteja definido ou esteja vazio
-    $h9_4 = 1;
+    $h9_4 = 0;
 }
 
 if (isset($_POST['g9_4']) && !empty($_POST['g9_4'])) {
     $g9_4 = virg($_POST['g9_4']);
 } else {
     // Ação caso 'g9_4' não esteja definido ou esteja vazio
-    $g9_4 = 1;
+    $g9_4 = 0;
 }
 
 if (isset($_POST['c9_4']) && !empty($_POST['c9_4'])) {
     $c9_4 = virg($_POST['c9_4']);
 } else {
     // Ação caso 'c9_4' não esteja definido ou esteja vazio
-    $c9_4 = 1;
+    $c9_4 = 0;
 }
 
 if (isset($_POST['i9_4']) && !empty($_POST['i9_4'])) {
     $i9_4 = virg($_POST['i9_4']);
 } else {
     // Ação caso 'i9_4' não esteja definido ou esteja vazio
-    $i9_4 = 1;
+    $i9_4 = 0;
 }
 
 if (isset($_POST['a9_4']) && !empty($_POST['a9_4'])) {
     $a9_4 = virg($_POST['a9_4']);
 } else {
     // Ação caso 'a9_4' não esteja definido ou esteja vazio
-    $a9_4 = 1;
+    $a9_4 = 0;
 }
 
 if (isset($_POST['ef9_4']) && !empty($_POST['ef9_4'])) {
     $ef9_4 = virg($_POST['ef9_4']);
 } else {
     // Ação caso 'ef9_4' não esteja definido ou esteja vazio
-    $ef9_4 = 1;
+    $ef9_4 = 0;
 }
 
 if (isset($_POST['r9_4']) && !empty($_POST['r9_4'])) {
     $r9_4 = virg($_POST['r9_4']);
 } else {
-    $r9_4 = 1;
+    $r9_4 = 0;
 }
 
-
 //media das materias do nono ano
-$ef9 = ((float)$ef9_1 + (float)$ef9_2 + (float)$ef9_3 + (float)$ef9_4) / 4;
-$lp9 = ($lp9_1 + $lp9_2 + $lp9_3 + $lp9_4) / 4;
-$ma9 = ($m9_1 + $m9_2 + $m9_3 + $m9_4) / 4;
-$hi9 = ($h9_1 + $h9_2 + $h9_3 + $h9_4) / 4;
-$ge9 = ($g9_1 + $g9_2 + $g9_3 + $g9_4) / 4;
-$ci9 = ($c9_1 + $c9_2 + $c9_3 + $c9_4) / 4;
-$li9 = ($i9_1 + $i9_2 + $i9_3 + $i9_4) / 4;
-$ar9 = ($a9_1 + $a9_2 + $a9_3 + $a9_4) / 4;
-$re9 = ($r9_1 + $r9_2 + $r9_3 + $r9_4) / 4;
+if ($d9 == 0) {
+
+    $ef9 = 1;
+} else {
+
+    $ef9 = ((float)$ef9_1 + (float)$ef9_2 + (float)$ef9_3 + (float)$ef9_4) / $d9;
+}
+$lp9 = ($lp9_1 + $lp9_2 + $lp9_3 + $lp9_4) / $n9;
+$ma9 = ($m9_1 + $m9_2 + $m9_3 + $m9_4) / $n9;
+$hi9 = ($h9_1 + $h9_2 + $h9_3 + $h9_4) / $n9;
+$ge9 = ($g9_1 + $g9_2 + $g9_3 + $g9_4) / $n9;
+$ci9 = ($c9_1 + $c9_2 + $c9_3 + $c9_4) / $n9;
+$li9 = ($i9_1 + $i9_2 + $i9_3 + $i9_4) / $n9;
+$ar9 = ($a9_1 + $a9_2 + $a9_3 + $a9_4) / $n9;
+$re9 = ($r9_1 + $r9_2 + $r9_3 + $r9_4) / $n9;
 
 //Média de todos os anos
-$ef = ((float)$ef6 + (float)$ef7 + (float)$ef8 + (float)$ef9) / 4;
+$ef = ((float)$ef6 + (float)$ef7 + (float)$ef8 + (float)$ef9) / $d;
 $lp = ($lp6 + $lp7 + $lp8 + $lp9) / 4;
 $ar = ($ar6 + $ar7 + $ar8 + $ar9) / 4;
 $li = ($li6 + $li7 + $li8 + $li9) / 4;
@@ -274,10 +282,6 @@ if ($ef == 0) {
     require_once('../models/model.php');
     $test = cadastrar($nome, $c1, $c2, $dn, $lp, $ar, $ef, $li, $ma, $ci, $ge, $hi, $re, $bairro, $publica, $pcd, $media);
 }
-
-require_once('../models/model.php');
-
-$test = cadastrar($nome, $c1, $c2, $dn, $lp, $ar, $ef, $li, $ma, $ci, $ge, $hi, $re, $bairro, $publica, $pcd, $media);
 
 switch ($test) {
     case 'candidato cadastrado com sucesso':
