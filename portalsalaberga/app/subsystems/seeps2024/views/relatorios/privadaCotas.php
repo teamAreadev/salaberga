dc5a7433eff71ed6114617668c716f56be10645a
Uchôa <uchoadev16@gmail.com>
2024-11-20 14:57:48 -0300

    classificaveis, classificados, publica cotas e privadas cotas

--- portalsalaberga/app/subsystems/seeps2024/views/relatorios/privadaCotas.php
+++ portalsalaberga/app/subsystems/seeps2024/views/relatorios/privadaCotas.php
@@ -18,13 +18,11 @@ function privadasCotas()
     // Cabeçalho com larguras ajustadas
     $pdf->Image('../../assets/images/logo.png', 8, 8, 15, 0, 'PNG');
     $pdf->SetFont('Arial', 'B', 25);
-    $pdf->Cell(185, 10, ('PUBLICA GERAL'), 0, 1, 'C');
+    $pdf->Cell(185, 10, ('PRIVADA COTA'), 0, 1, 'C');
     $pdf->SetFont('Arial', 'B', 8);
-    $pdf->Cell(321, 0, ('PCD = PESSOA COM DEFICIENCIA'), 0, 1, 'C');
-    $pdf->Cell(315, -10, ('AC = AMPLA CONCORRENCIA'), 0, 1, 'C');
-    $pdf->Cell( 332, 0, ('COTISTA = INCLUSO NA COTA DO BAIRRO'), 0, 1, 'C');
+    $pdf->Cell(0, 10, ('PCD = PESSOA COM DEFICIENCIA | COTISTA = INCLUSO NA COTA DO BAIRRO | AC = AMPLA CONCORRENCIA'), 0, 1, 'C');
     $pdf->SetFont('Arial', 'b', 12);
-    $pdf->Cell(185, 25, '', 0, 1, 'C');
+    $pdf->Cell(185, 10, '', 0, 1, 'C');
 
     // Fonte do cabeçalho
     $pdf->SetFont('Arial', 'B', 12);
