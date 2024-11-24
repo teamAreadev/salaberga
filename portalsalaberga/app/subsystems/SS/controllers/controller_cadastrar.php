0e7aef4b44642bd6e44c9de141fd7ecc4d2ceea8
DevRoger7 <cavalcanterogeer@gmail.com>
2024-11-24 02:02:31 -0300

    cadastrar funcionando
    fiz o cadastrar e criei o redirecionamento de acordo com o status da conta

--- portalsalaberga/app/subsystems/SS/controllers/controller_cadastrar.php
+++ portalsalaberga/app/subsystems/SS/controllers/controller_cadastrar.php
@@ -1,11 +1,12 @@
 <?php
 
-if (isset($_POST['nomeC']) && isset($_POST['cargo']) && isset($_POST['email']) && isset($_POST['senha']) && isset($_POST['status'])){
+if (isset($_POST['nomeC']) && isset($_POST['UserName']) && isset($_POST['email']) && isset($_POST['senha']) && isset($_POST['status']) && isset($_POST['cargo'])){
     $nomeC = $_POST['nomeC'];
-    $cargo = $_POST['cargo'];
+    $userName = $_POST['UserName'];
     $email = $_POST['email'];
     $senha = $_POST['senha'];
     $status = $_POST['status'];
+    $cargo = $_POST['cargo'];
     require_once('../models/model.php');
-    cadastrarUsuario($nomeC, $cargo, $email, $senha, $status);
+    cadastrarUsuario($nomeC, $userName, $email, $senha, $status, $cargo);
 }
--- portalsalaberga/app/subsystems/SS/models/model.php
+++ portalsalaberga/app/subsystems/SS/models/model.php
@@ -1,21 +1,44 @@
 <?php
     
 
-    function cadastrarUsuario($nomeC, $cargo, $email, $senha, $status)
-    {
+    function cadastrarUsuario($nomeC, $userName, $email, $senha, $status, $cargo)
+{
+    try {
         require_once('../config/connect.php');
-        //inserido na tabela candidato os dados do candidato
+        
+        // Primeiro, verifica se o email já existe
+        $stmtCheck = $conexao->prepare("SELECT COUNT(*) FROM usuario WHERE email = :email");
+        $stmtCheck->bindValue(':email', $email);
+        $stmtCheck->execute();
+        
+        if ($stmtCheck->fetchColumn() > 0) {
+            throw new Exception("Este email já está cadastrado no sistema");
+        }
+        
+        // Se o email não existe, procede com a inserção
         $stmtInsert = $conexao->prepare('
-        INSERT INTO usuario (nome, email, senha, cargo, status) VALUES (:nomeC, :email, MD5(:senha), :cargo, :status)
+            INSERT INTO usuario (username, nome, email, senha, cargo, status) 
+            VALUES (:UserName, :nomeC, :email, MD5(:senha), :cargo, :status)
         ');
+        
         $stmtInsert->bindValue(':nomeC', $nomeC);
+        $stmtInsert->bindValue(':UserName', $userName);
         $stmtInsert->bindValue(':email', $email);
         $stmtInsert->bindValue(':senha', $senha);
         $stmtInsert->bindValue(':cargo', $cargo);
         $stmtInsert->bindValue(':status', $status);
-        $stmtInsert->execute();
-
+        
+        return $stmtInsert->execute();
+        
+    } catch (PDOException $e) {
+        // Verifica se é um erro de duplicidade
+        if ($e->getCode() == '23000') {
+            throw new Exception("Este email já está cadastrado no sistema");
+        }
+        error_log("Erro ao cadastrar usuário: " . $e->getMessage());
+        throw new Exception("Erro ao cadastrar usuário no sistema");
     }
+}
     function cadastrar($nome, $c1, $c2, $dn, $lp, $ar, $ef, $li, $ma, $ci, $ge, $hi, $re, $bairro, $publica, $pcd, $media)
     {
         require_once('../config/connect.php');
@@ -129,7 +152,7 @@
         $result_logar->bindValue(':senha', $senha);
         $result_logar->execute();
         $result = $result_logar->fetchAll(PDO::FETCH_ASSOC);
-        
+        print_r($result);
         
         //se for o result_logar for maior que 0
         foreach ($result as $key) {
