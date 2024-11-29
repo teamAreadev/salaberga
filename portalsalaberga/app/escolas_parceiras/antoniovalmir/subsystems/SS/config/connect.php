7ee93dd9f8a30cee3d36a4b1a344a4828734a066
DevRoger7 <cavalcanterogeer@gmail.com>
2024-11-29 20:38:24 -0300

    mudando link banco antonio valmir

--- portalsalaberga/app/escolas_parceiras/antoniovalmir/subsystems/SS/config/connect.php
+++ portalsalaberga/app/escolas_parceiras/antoniovalmir/subsystems/SS/config/connect.php
@@ -1,13 +1,9 @@
 <?php
-/*
+
 $dsn = 'mysql:host=localhost;dbname=u750204740_Maria_Carmem';
 $username = "u750204740_Antonio_Valmir";
 $password = "Antonho_Valmir!@##qwe123";
-*/
 
-$dsn = 'mysql:host=localhost;dbname=ss';
-$username = "root";
-$password = "";
 
 try {
     $conexao = new PDO($dsn, $username, $password);
